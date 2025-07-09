2612661116962febc961dde3eb490ec858a47a1f
Lukas Eichenauer <leichenauer@databay.de>
2025-07-09 15:13:37 +0200

    refactor: adjust settings repositories

--- components/ILIAS/Test/src/Settings/MainSettings/class.SettingsMainGUI.php
+++ components/ILIAS/Test/src/Settings/MainSettings/class.SettingsMainGUI.php
@@ -426,6 +426,7 @@ class SettingsMainGUI extends TestSettingsGUI
         );
 
         $settings = new MainSettings(
+            $this->main_settings->getId(),
             $general_settings,
             $introduction_settings,
             $access_settings,

6ecd96bcf78db58e596725c4a230406ed39a71e7
Matheus Zych <mzych@databay.de>
2025-09-18 07:52:32 +0200

    Adds tiny fix to request question pool request data collector

--- components/ILIAS/TestQuestionPool/src/RequestDataCollector.php
+++ components/ILIAS/TestQuestionPool/src/RequestDataCollector.php
@@ -273,6 +273,6 @@ class RequestDataCollector
             $chain = $this->refinery->kindlyTo()->dictOf($chain);
         }
 
-        return $this->get($key, $this->refinery->byTrying([$chain, $this->refinery->always([])]));
+        return $this->get($key, $this->refinery->byTrying([$chain, $this->refinery->always([])])) ?? [];
     }
 }
